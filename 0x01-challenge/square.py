44517692bfa441daab3279d205b6d940ad0f6d78
Xvaria <Xvongola23@gmail.com>
2021-05-21 01:22:30 -0500

    Ready | My square

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -1,22 +1,28 @@
 #!/usr/bin/python3
+'''My square'''
 
 
 class square():
+    '''I love geometry!'''
     width = 0
     height = 0
 
     def __init__(self, *args, **kwargs):
+        '''Method initialize'''
         for key, value in kwargs.items():
             setattr(self, key, value)
 
     def area_of_my_square(self):
+        '''Method Area'''
         """ Area of the square """
         return self.width * self.height
 
     def PermiterOfMySquare(self):
+        '''Method Perimeter'''
         return (self.width * 2) + (self.height * 2)
 
     def __str__(self):
+        '''Method to print'''
         return "{}/{}".format(self.width, self.height)
 
 if __name__ == "__main__":
