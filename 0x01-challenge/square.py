8052471a5b6973a11ee1f96e1177d6147b8af7f3
Xvaria <Xvongola23@gmail.com>
2021-05-24 00:10:54 -0500

    Correction v2.0 | Fix typo

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -4,16 +4,15 @@
 
 class square():
     '''I love geometry!'''
-    width = 0
-    height = 0
 
     def __init__(self, *args, **kwargs):
         '''Method initialize'''
+        self.width = 0
+        self.height = 0
         for key, value in kwargs.items():
             setattr(self, key, value)
 
     def area_of_my_square(self):
-        '''Method Area'''
         """ Area of the square """
         return self.width * self.height
 
@@ -26,8 +25,7 @@ class square():
         return "{}/{}".format(self.width, self.height)
 
 if __name__ == "__main__":
-
-    s = square(width=12, height=12)
+    s = square(width=12, height=9)
     print(s)
     print(s.area_of_my_square())
     print(s.PermiterOfMySquare())
